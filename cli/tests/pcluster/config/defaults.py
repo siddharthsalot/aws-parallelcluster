80a569ff53e25cc6e2e689118d93e8514efbb603
Tim Lane <tilne@amazon.com>
2020-04-21 17:18:23 -0700

    Add Arch CFN parameter required for ARM support
    This commit adds the Arch CFN parameter to the configuration code.
    This new parameter is a derived entirely from other parameters in the
    configuration file, and is thus not permitted to be configured there.
    
    Also included are validators that cause cluster creation to fail fast
    when the architecture-specific components are not compatible with each
    other or with the features enabled by the configuration file. The
    specific validators are:
    * Ensure the master and compute instance types have compatible instance
      architectures
    * Ensure a custom AMI supports the architecture implied by the instance
      types
    * Ensure that ARM instances are used only on OSs for which ARM base
      AMIs are available
    * Ensure that incompatible features like disabling hyperthreading, FSx,
      or the Intel HPC platform are not enabled when using ARM instance
      types
    
    This commit also adds the Arch parameter to the CFN template and
    modifies AMI selection such that a architecture-specific mapping is
    used depending on the value of this new parameter.
    
    Signed-off-by: Tim Lane <tilne@amazon.com>

--- cli/tests/pcluster/config/defaults.py
+++ cli/tests/pcluster/config/defaults.py
@@ -136,6 +136,7 @@ DEFAULT_CLUSTER_DICT = {
     "dcv_settings": None,
     "cw_log_settings": None,
     "cluster_config_metadata": {"sections": {}},
+    "architecture": "x86_64",
 }
 
 DEFAULT_CW_LOG_DICT = {"enable": True, "retention_days": 14}
@@ -164,7 +165,7 @@ class DefaultDict(Enum):
 # ------------------ Default CFN parameters ------------------ #
 
 # number of CFN parameters created by the PclusterConfig object.
-CFN_CONFIG_NUM_OF_PARAMS = 58
+CFN_CONFIG_NUM_OF_PARAMS = 59
 
 # CFN parameters created by the pcluster CLI
 CFN_CLI_RESERVED_PARAMS = ["ResourcesS3Bucket"]
@@ -273,6 +274,8 @@ DEFAULT_CLUSTER_CFN_PARAMS = {
     # cw_log_settings
     "CWLogOptions": "true,14",
     "ClusterConfigMetadata": "{'sections': {}}",
+    # architecture
+    "Architecture": "x86_64",
 }
 
 
